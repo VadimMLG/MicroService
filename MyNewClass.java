415da0778a9eb6958ac8cbcb9201502b918e0e48
Vadim <vadimlg888@gmail.com>
2025-09-09 14:15:47 +0300

    Добавил новый метод printMessage()

--- MyNewClass.java
+++ MyNewClass.java
@@ -1,5 +1,11 @@
 public class MyNewClass {
     public static void main(String[] args) {
         System.out.println("Мой новый класс в MicroService!");
+        printMessage(); // Вызов нового метода
+    }
+    
+    // Добавляем новый метод
+    public static void printMessage() {
+        System.out.println("Это новое изменение в классе!");
     }
 }
